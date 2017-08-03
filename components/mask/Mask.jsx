9a777df68d60801e7915fbdfb286d19d69cf16c8
zhhyang <zhhyang0404@gmail.com>
2017-08-03 16:52:40 +0800

    release 0.7.3    当Mask遮罩层打开时，自动禁用body的滚动

--- components/mask/Mask.jsx
+++ components/mask/Mask.jsx
@@ -1,7 +1,7 @@
 /**
  * Created by freeman on 17-5-25.
  */
-import React from 'react';
+import React ,{Component}from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
@@ -9,17 +9,44 @@ import classNames from 'classnames';
  * screen mask, use in `Dialog`, `ActionSheet`, `Popup`.
  *
  */
-const Mask = props => {
+class Mask extends Component{
 
-  const {transparent, className, prefixCls, ...others} = props;
-  const clz = classNames({
-    [prefixCls]: !transparent,
-    [`${prefixCls}_transparent`]: transparent
-  }, className);
+  constructor (props) {
+    super(props)
+    this.show = this.show.bind(this)
+    this.destroy = this.destroy.bind(this)
+  }
 
-  return (
-    <div className={clz} {...others}/>
-  );
+  destroy() {
+    // Use regex to prevent matching `mask-open` as part of a different class, e.g. `my-mask-opened`
+    const classes = document.body.className.replace(/(^| )mask-open( |$)/, ' ');
+    document.body.className = classNames(classes).trim();
+  }
+
+  show() {
+    const classes = document.body.className;
+    document.body.className = classNames(classes,'mask-open');
+  }
+
+  componentDidMount() {
+    this.show()
+  }
+
+  componentWillUnmount() {
+    this.destroy();
+  }
+  render(){
+    const {transparent, className, prefixCls, ...others} = this.props;
+    const clz = classNames({
+      [prefixCls]: !transparent,
+      [`${prefixCls}_transparent`]: transparent
+    }, className);
+
+    return (
+      <div className={clz} {...others}/>
+    );
+  }
+
 }
 Mask.propTypes = {
   prefixCls: PropTypes.string,
